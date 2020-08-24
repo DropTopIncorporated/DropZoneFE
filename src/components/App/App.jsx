d401860b2bd23ba2657dbc7df69cffe1b855cb18
Lucia Brammer <brammerl96@gmail.com>
2020-08-24 11:36:12 -0700

    outline complete

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import GamesContainer from '../../containers/GamesContainer';
+import Header from '../Header/Header';
 
 export default function App() {
   return (
     <>
+    <Header />
     <GamesContainer />
     </>);
 }
