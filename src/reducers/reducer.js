1864357eb6270a0a322d8b342d4d46c2a7a1ac25
Erik Ford <ekford18@gmail.com>
2020-08-25 10:39:14 -0700

    streamer component

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,23 +1,23 @@
-import { SET_GAMES, SET_CHANNELS, SET_FAVORITES } from "../actions/reducerActions"
+import { SET_GAMES, SET_CHANNELS, SET_FAVORITES } from '../actions/reducerActions';
 
 export const initialState = {
   games: [],
   streams:[],
   channels: [],
   favorites: []
-}
+};
 
 export default function reducer(state, action) {
   switch(action.type) {
     case SET_GAMES: {
-      return { ...state, games: action.payload}
-    };
+      return { ...state, games: action.payload };
+    }
     case SET_CHANNELS: {
-      return { ...state, channels: action.payload}
-    };
+      return { ...state, channels: action.payload };
+    }
     case SET_FAVORITES: {
       return { ...state, favorites: action.payload 
-      }
+      };
     }
   }
-};
+}
