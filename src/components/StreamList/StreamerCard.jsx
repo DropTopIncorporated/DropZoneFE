99acf8fd8acc67c49a7f938a120f47c4df4e0300
Lucia Brammer <brammerl96@gmail.com>
2020-08-25 16:31:16 -0700

    more css styling

--- src/components/StreamList/StreamerCard.jsx
+++ src/components/StreamList/StreamerCard.jsx
@@ -7,28 +7,17 @@ import CardMedia from '@material-ui/core/CardMedia';
 import styles from './StreamerCard.css'
 
 
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
-    background: '#6B6E70',
-    color: '#474B4F',
-    border: '3px solid green',
-    borderRadius: '20px'
-  },
-});
 
 export default function StreamerCard({ streamTitle, streamerName, viewerCount, image }) {
-  const classes = useStyles();
-
   return (
-    <Card className={classes.root} onClick={() => window.open(`https://twitch.tv/${streamerName}`)}>
+    <Card className={styles.card} onClick={() => window.open(`https://twitch.tv/${streamerName}`)}>
       <CardActionArea>
         <CardMedia
           component="img"
           alt={streamerName}
           image={image}
         />
-        <CardContent>
+        <CardContent className={styles.caption}>
           <h3 className={styles.streamerTitle}>
             {streamTitle},
           </h3>
