b97a75e47cfe57e39d10f85b8fbf8eec2c3734c9
Jpendy <Jpendy256@gmail.com>
2020-08-26 17:15:39 -0700

    made it work

--- src/services/apiFetches.js
+++ src/services/apiFetches.js
@@ -8,17 +8,36 @@ export const fetchGamesWithDrops = (search = '') => {
 
 export const fetchStreamers = (title, search = '') => {
   return fetch(`${localUrl}/streams/${title}?search=${search}`)
-    // .then(console.log)
     .then(res => res.json());
 };
 
 export const fetchAllStreamers = () => {
-  return fetch(`${url}/streams`)
+  return fetch(`${localUrl}/streams`)
     .then(res => res.json());
 };
 
-// export const fetchSearch = (search) => {
-//   return fetch(`${localUrl}/games?search=${search}`)
-//     // .then(console.log)
-//     .then(res => res.json());
-// };
+export const postFavorite = body => {
+  return fetch(`${localUrl}/favorites`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    credentials: 'include',
+    body: JSON.stringify(body)
+  })
+    .then(res => res.json());
+};
+
+export const getAllFavorites = () => {
+  return fetch(`${localUrl}/favorites`, {
+    method: 'GET',
+    credentials: 'include'
+  })
+    .then(res => res.json());
+};
+
+export const deleteFavorite = title => {
+  return fetch(`${localUrl}/favorites/${title}`, {
+    method: 'DELETE',
+    credentials: 'include'
+  })
+    .then(res => res.json());
+};
