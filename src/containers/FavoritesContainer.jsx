77b3c269c4ab91efa972c2b5bcff2a354d8dc66f
Jpendy <Jpendy256@gmail.com>
2020-08-27 11:27:14 -0700

    added delete button and links to streams to favorite page

--- src/containers/FavoritesContainer.jsx
+++ src/containers/FavoritesContainer.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { getAllFavorites } from '../services/apiFetches';
+import { Link } from 'react-router-dom';
+import { getAllFavorites, deleteFavorite } from '../services/apiFetches';
 import { useSelector } from '../hooks/AppContext';
 import { getGames } from '../selectors/selectors';
 
 export default function FavoritesContainer() {
   const [favoritesList, setFavoritesList] = useState([]);
+  const [changeFavorites, setChangeFavorites] = useState(false);
+
   const games = useSelector(getGames);
 
   useEffect(() => {
@@ -12,16 +15,29 @@ export default function FavoritesContainer() {
       .then(favorites => favorites.map(item => item.gameTitle))
     //   .then(res => [...new Set(res)])
       .then(setFavoritesList);
-  }, []);
+  }, [changeFavorites]);
+
+  const handleClick = (title) => {
+
+    deleteFavorite(title)
+      .then(res => {
+        setFavoritesList(favoritesList.filter(item => item.gameTitle !== res.gameTitle));
+        changeFavorites ? setChangeFavorites(false) : setChangeFavorites(true);
+      });
+
+  };
 
   const gamesList = games.filter(({ title }) => favoritesList.includes(title));
 
   const listElements = gamesList.map(({ title, name, imageUrl }, i) => {
     return (
       <li key={i} >
-        <img src={imageUrl} />
+        <Link to={`/${title}`} >
+          <img src={imageUrl} />
+        </Link>
         <p>{name}</p>
-        <p>{title}</p>       
+        <p>{title}</p>
+        <button onClick={() => handleClick(title)} >Delete</button>       
       </li>
     );
   });
