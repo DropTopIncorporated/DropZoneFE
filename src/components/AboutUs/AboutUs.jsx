c051d34d8a782f030d909da2e294502e0e58d2ae
Erik Ford <ekford18@gmail.com>
2020-08-27 09:20:15 -0700

    aboutus page updated

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -10,30 +10,30 @@ const AboutUs = () => {
         <div className={styles.profileArea}>
           <img className={styles.profileImage} />
           <h2>Dan Bennington</h2>
+          <p className={styles.textArea}>Description of why chose project and if you could stream one thing what it would be?</p>
           <div>
-            <a href="https://www.linkedin.com/in/erik-ford-business/">
-              <img src={linkedIn} />
+            <a href="https://www.linkedin.com/in/dan-bennington/">
+              <img className={styles.linkImage} src={linkedIn} />
             </a>
-            <a href="https://github.com/Morriden">
-              <img src={Github} />
+            <a href="https://github.com/dbennin125">
+              <img className={styles.linkImage} src={Github} />
             </a>
           </div>
-          <p>Description of why chose project and if you could stream one thing what it would be?</p>
         </div>
       </section>
       <section >
         <div className={styles.profileArea}>
           <img className={styles.profileImage} />
           <h2>Lucia Brammer</h2>
+          <p className={styles.textArea}>Description of why chose project and if you could stream one thing what it would be?</p>
           <div>
-            <a href="https://www.linkedin.com/in/erik-ford-business/">
-              <img src={linkedIn} />
+            <a href="https://www.linkedin.com/in/luciabrammer/">
+              <img className={styles.linkImage} src={linkedIn} />
             </a>
             <a href="https://github.com/Morriden">
-              <img src={Github} />
+              <img className={styles.linkImage} src={Github} />
             </a>
           </div>
-          <p>Description of why chose project and if you could stream one thing what it would be?</p>
         </div>
       </section>
       <section >
@@ -41,15 +41,15 @@ const AboutUs = () => {
         <div className={styles.profileArea}>
           <img className={styles.profileImage}/>
           <h2>Erik Ford</h2>
+          <p className={styles.textArea}>Description of why chose project and if you could stream one thing what it would be?</p>
           <div>
             <a href="https://www.linkedin.com/in/erik-ford-business/">
-              <img src={linkedIn} />
+              <img className={styles.linkImage} src={linkedIn} />
             </a>
             <a href="https://github.com/Morriden">
-              <img src={Github} />
+              <img className={styles.linkImage} src={Github} />
             </a>
           </div>
-          <p>Description of why chose project and if you could stream one thing what it would be?</p>
         </div>
         
       </section>
@@ -57,30 +57,30 @@ const AboutUs = () => {
         <div className={styles.profileArea}>
           <img className={styles.profileImage} />
           <h2>Max Lamb</h2>
+          <p className={styles.textArea}>Description of why chose project and if you could stream one thing what it would be?</p>
           <div>
-            <a href="https://www.linkedin.com/in/erik-ford-business/">
-              <img src={linkedIn} />
+            <a href="https://www.linkedin.com/in/max-lamb/">
+              <img className={styles.linkImage} src={linkedIn} />
             </a>
-            <a href="https://github.com/Morriden">
-              <img src={Github} />
+            <a href="https://github.com/MaximusLamb">
+              <img className={styles.linkImage} src={Github} />
             </a>
           </div>
-          <p>Description of why chose project and if you could stream one thing what it would be?</p>
         </div>
       </section>
       <section >
         <div className={styles.profileArea}>
           <img className={styles.profileImage} />
           <h2>Jake Pendergraft</h2>
+          <p className={styles.textArea}>Description of why chose project and if you could stream one thing what it would be?</p>
           <div>
-            <a href="https://www.linkedin.com/in/erik-ford-business/">
-              <img src={linkedIn} />
+            <a href="https://www.linkedin.com/in/jake-pendergraft/">
+              <img className={styles.linkImage} src={linkedIn} />
             </a>
-            <a href="https://github.com/Morriden">
-              <img src={Github} />
+            <a href="https://github.com/Jpendy">
+              <img className={styles.linkImage} src={Github} />
             </a>
           </div>
-          <p>Description of why chose project and if you could stream one thing what it would be?</p>
         </div>
       </section>
     </div>
