ea47cc876596c226979e2bb9ff06a56f9998d58a
Lucia Brammer <brammerl96@gmail.com>
2020-08-26 17:44:40 -0700

    ugh

--- src/components/StreamList/StreamerCard.jsx
+++ src/components/StreamList/StreamerCard.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import styles from './StreamerCard.css';
